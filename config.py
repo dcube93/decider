a958dcb395932bf47bb3628de023bdfa9ef1c50c
dcube93 <dominik_wirz@hotmail.com>
2023-03-23 13:06:33 +0100

    config

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+# Hier werden Konfigurations Objekte definiert.
+# Quelle: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
+# Quelle: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+# Quelle https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xv-a-better-application-structure
+
 import os
 from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-# Hier werden Konfigurations Objekte definiert.
-# Quelle: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
-# Quelle: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
-# Quelle https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xv-a-better-application-structure
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
