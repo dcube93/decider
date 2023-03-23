4cac6d22ce84a46711f467d2316514519fd8416b
dcube93 <dominik_wirz@hotmail.com>
2023-03-23 10:34:06 +0100

    kommentiert

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Hier werden Konfigurations Objekte definiert.
+# Quelle: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
+# Quelle: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
